6a018929d74d653e83b257e8826b0448a93d7997
Alan Cristhian <alan.cristh@gmail.com>
2015-02-27 20:00:13 -0300

    Inital commit

---
+++ install_python34_dependencies.sh
@@ -0,0 +1,6 @@
+python3.4 -m pip install --user bottle
+python3.4 -m pip install --user peewee
+python3.4 -m pip install --user pony
+python3.4 -m pip install --user wand
+python3.4 -m pip install --user webtest
+python3.4 -m pip install --user html5lib
---
+++ install_python34_dependencies_local.sh
@@ -0,0 +1,6 @@
+~/env/py34/bin/python3.4 -m pip install bottle
+~/env/py34/bin/python3.4 -m pip install peewee
+~/env/py34/bin/python3.4 -m pip install pony
+~/env/py34/bin/python3.4 -m pip install wand
+~/env/py34/bin/python3.4 -m pip install webtest
+~/env/py34/bin/python3.4 -m pip install html5lib
